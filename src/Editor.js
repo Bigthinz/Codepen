1d90d00fd4f4701561c767a79342460d711828f5
Bigthinz <bigthinzcount@gmail.com>
2021-06-08 00:13:52 +0100

    editor js

--- src/Editor.js
+++ src/Editor.js
@@ -1,10 +1,37 @@
 import React from "react";
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+
+export default function Editor(props) {
+
+  const {
+    language,
+    displayName
+  } = props
 
 
-export default function App() {
   return (
     <>
-     
+     <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>o/c</button>
+      </div>
+      <ControlledEditor 
+        onBeforeChange={handleChange}
+        value={value}
+        className='code-mirror-wrapper'
+        options={{
+          lineWrapping= true,
+          lint= true,
+          mode=language
+        }}
+      />
+     </div>
     </>
   );
 }
